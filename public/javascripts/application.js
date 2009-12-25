efb46efaa3752d441daf71ab5ac3f6bb98c790e0
jollyroger <jollyroger@blasta.(none)>
2009-12-25 18:25:27 +0100

    fixed asset_packager deployment bug

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function(){
-  jQuery('form#new_idea').submit(function(){
-    alert('boho');
-    jQuery(this).validate();
-  });
+  // TODO this
+  // jQuery('form.validate').validate();
+  // as a generic form validator is now working, no errors in the console
+  jQuery('form#new_idea').validate();
+  jQuery('form#new_user').validate();
+  jQuery('form#new_user_session').validate();
 });

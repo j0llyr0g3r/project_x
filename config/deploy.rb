f7985c01755f4fd89b5468fc7f43dfddc60e191a
jollyroger <jollyroger@blasta.(none)>
2009-12-28 05:35:15 +0100

    improved deployment by symlinking the sphinx config to shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,13 +50,27 @@ namespace :sass do
 end
 
 namespace :deploy do
+  task :re_index do
+    run "cd #{current_path} &&  rake ts:index RAILS_ENV=#{stage}"
+  end
   task :restart do
     run "touch #{current_path}/tmp/restart.txt" 
   end
   task :symlink_shared do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    # why do we do this?
+    # The reason is, that thinking sphinx is running from withing our
+    # current deployment directory like "searchd --config .....
+    # On a new deployment, TS would still be running, but with the
+    # old config-file from our previous deployment folder - which would be an
+    # error-prone, unclean solution. 
+    # So we have two options:
+    #   1. stopping TS, re-generating the config-file and restarting TS
+    #   2. symlinking the config-file from the shared folder -> better
+    run "ln -nfs #{shared_path}/config/production.sphinx.conf #{release_path}/config/production.sphinx.conf"
   end
 end
 
 after 'deploy:symlink', 'deploy:symlink_shared'
-after 'deploy:symlink', 'sass:update'
+after 'deploy:symlink_shared', 'sass:update'
+after 'sass:update', 'deploy:re_index'

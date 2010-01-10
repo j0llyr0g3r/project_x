c81b451342b6db72032c36f4c68ad2d43c547136
Timo Roessner <troessner@jovoto.com>
2010-01-10 17:08:33 +0100

    removed the rest of authlogic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ActiveRecord::Base
-  attr_accessible :login, :email, :password, :password_confirmation, :first_name, :last_name
-  acts_as_authentic
-  validates_presence_of :login, :email
-  validates_uniqueness_of :login, :email
+  #attr_accessible :login, :email, :password, :password_confirmation, :first_name, :last_name
+  #devise :all
+#  validates_presence_of :login, :email
+#  validates_uniqueness_of :login, :email
 
   has_many :leaderships, :through => :inspirations, :class_name => 'Idea', :source => :idea, :uniq => true
   has_many :inspirations
 
   has_many :memberships, :through => :collaborations, :class_name => 'Idea', :source => :idea, :uniq => true
   has_many :collaborations
+
+  def create_leadership(args)
+    leaderships.create(args)
+  end
+
 end
--- db/migrate/20091223032507_create_users.rb
+++ db/migrate/20091223032507_create_users.rb
@@ -4,19 +4,14 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :login
       t.string :email
-      t.string :crypted_password
-      t.string :password_salt
-      t.string    :persistence_token,   :null => false
-      t.integer   :login_count,         :null => false, :default => 0
-      t.integer   :failed_login_count,  :null => false, :default => 0
-      t.datetime  :last_request_at                                   
-      t.datetime  :current_login_at                                  
-      t.datetime  :last_login_at                                     
-      t.string    :current_login_ip                                  
-      t.string    :last_login_ip
       t.string :first_name
       t.string :last_name
-      t.timestamps
+#      t.authenticatable
+#      t.confirmable
+#      t.recoverable
+#      t.rememberable
+#      t.trackable
+#      t.timestamps
     end
   end
   
